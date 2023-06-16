f70b8317bfb046aec0eb369bb285a8e12f7113c1
Tejas Dhodi <tejasdhodi77@gmail.com>
2023-06-16 15:53:36 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("Jay Shree Ram");
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const navItems = document.querySelector(".navItems");
+const navLIst = document.querySelectorAll(".navList");
+
+const certificateContainer = [...document.querySelectorAll(".certificateContainer")];
+const preButton = [...document.querySelectorAll(".pre")];
+const nxtButton = [...document.querySelectorAll(".next")];
+
+// To show navbar on click
+hamburgerMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("toggleHamb")
+    navItems.classList.toggle("showNav")
+})
+
+// To reverse the navbar
+navLIst.forEach((items) => {
+    items.addEventListener("click", () => {
+        hamburgerMenu.classList.remove("toggleHamb")
+    navItems.classList.remove("showNav")
+    })
+})
+
+// To add Scroll effect on carousel
+
+certificateContainer.forEach((item, i) => {
+    let containerDimension = item.getBoundingClientRect();
+    let containerWidth = containerDimension.width;
+  
+    nxtButton[i].addEventListener("click", () => {
+      item.scrollLeft += containerWidth - 1;
+    });
+  
+    preButton[i].addEventListener("click", () => {
+      item.scrollLeft -= containerWidth;
+    });
+  });
