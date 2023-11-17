d1a5c88c27aa229f7046ae901c5b115a6d598022
Tejas Dhodi <tejasdhodi77@gmail.com>
2023-11-17 17:51:50 +0530

    Update Responsiveness

--- index.js
+++ index.js
@@ -2,36 +2,41 @@ console.log("Jay Shree Ram");
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
 const navItems = document.querySelector(".navItems");
 const navLIst = document.querySelectorAll(".navList");
-
+let navLinks = document.querySelectorAll("nav a");
+let sections = document.querySelectorAll("section");
 const certificateContainer = [...document.querySelectorAll(".certificateContainer")];
 const preButton = [...document.querySelectorAll(".pre")];
 const nxtButton = [...document.querySelectorAll(".next")];
 
+
+
+
 // To show navbar on click
 hamburgerMenu.addEventListener("click", () => {
-    hamburgerMenu.classList.toggle("toggleHamb")
-    navItems.classList.toggle("showNav")
+  hamburgerMenu.classList.toggle("toggleHamb")
+  navItems.classList.toggle("showNav")
 })
 
 // To reverse the navbar
 navLIst.forEach((items) => {
-    items.addEventListener("click", () => {
-        hamburgerMenu.classList.remove("toggleHamb")
+  items.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("toggleHamb")
     navItems.classList.remove("showNav")
-    })
-})
+  })
+});
+
 
 // To add Scroll effect on carousel
 
 certificateContainer.forEach((item, i) => {
-    let containerDimension = item.getBoundingClientRect();
-    let containerWidth = containerDimension.width;
-  
-    nxtButton[i].addEventListener("click", () => {
-      item.scrollLeft += containerWidth - 1;
-    });
-  
-    preButton[i].addEventListener("click", () => {
-      item.scrollLeft -= containerWidth;
-    });
+  let containerDimension = item.getBoundingClientRect();
+  let containerWidth = containerDimension.width;
+
+  nxtButton[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth + 100;
   });
+
+  preButton[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
